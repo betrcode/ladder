8edfa6b74a8e3cc0345cadf4630ce28773dc55ca
Max Wenzin <max.wenzin@gmail.com>
2016-06-11 13:27:36 +0200

    Initial commit. Ladder lab implemented.

---
+++ src/main/java/se/bettercode/ladder/LadderApplication.java
@@ -0,0 +1,52 @@
+package se.bettercode.ladder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+/**
+ * Takes a roof height and a ladder angle and calculates the needed length of the ladder to reach the roof.
+ */
+public class LadderApplication {
+  public static void main(String[] args) {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    PrintStream out = new PrintStream(System.out);
+    main(in, out);
+  }
+
+  /**
+   * Testable method
+   */
+  public static void main(BufferedReader in, PrintStream out) {
+    Entry<Integer, Integer> entry = readInput(in);
+    final double ladderLength = getLadderLength(entry.getKey(), entry.getValue());
+    printOutput(ladderLength, out);
+  }
+
+  /**
+   * Reads input from System.in
+   * Note: No error handling. Assumes two integers separated by space. Will crash on other values.
+   */
+  private static Entry<Integer, Integer> readInput(BufferedReader reader) {
+    final Scanner in = new Scanner(reader);
+    final String line = in.nextLine();
+    final String[] split = line.split("\\s+");
+    //Using a Map Entry here but a Apache Commons Pair would be nicer
+    return new SimpleImmutableEntry<>(new Integer(split[0]), new Integer(split[1]));
+  }
+
+  /**
+   * Calculates the needed length of the ladder using Pythagorean theorem, sin(angle) = roofHeight / ladderLength
+   */
+  private static double getLadderLength(Integer roofHeight, Integer angle) {
+    return roofHeight / Math.sin(Math.toRadians(angle));
+  }
+
+  private static void printOutput(double ladderLength, PrintStream out) {
+    out.println((int)Math.ceil(ladderLength));
+  }
+
+}
---
+++ src/test/java/se/bettercode/ladder/LadderApplicationTest.java
@@ -0,0 +1,34 @@
+package se.bettercode.ladder;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringReader;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests the ladder application using command line input and verifying output.
+ * A classic system test with a blackbox approach.
+ */
+public class LadderApplicationTest {
+
+  LadderApplication ladderApplication = new LadderApplication();
+  ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+  @Test
+  public void runApp() {
+    runMainWithInputAndAssertOutput("500 70", "533\n");
+    runMainWithInputAndAssertOutput("1000 10", "5759\n");
+    runMainWithInputAndAssertOutput("2000 70", "2129\n");
+  }
+
+  private void runMainWithInputAndAssertOutput(String input, String expectedOutput) {
+    out.reset();
+    BufferedReader in = new BufferedReader(new StringReader(input));
+    ladderApplication.main(in, new PrintStream(out));
+    assertEquals(expectedOutput, out.toString());
+  }
+}
